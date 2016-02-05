1a2b014e8c100283678da945e484c5b2eb5c22d8
Timm Bäder <mail@baedert.org>
2016-02-05 10:00:46 +0100

    Remove more unnecessary redraws
    The call to gtk_widget_set_state_flags immediately before these already
    queues a redraw/allocate/resize in case they have to be queued.

--- gtk/gtklistbox.c
+++ gtk/gtklistbox.c
@@ -1853,7 +1853,6 @@ gtk_list_box_multipress_gesture_pressed (GtkGestureMultiPress *gesture,
       gtk_widget_set_state_flags (GTK_WIDGET (priv->active_row),
                                   GTK_STATE_FLAG_ACTIVE,
                                   FALSE);
-      gtk_widget_queue_draw (GTK_WIDGET (box));
 
       if (n_press == 2 && !priv->activate_single_click)
         gtk_list_box_activate (box, row);
@@ -1929,7 +1928,6 @@ gtk_list_box_multipress_gesture_released (GtkGestureMultiPress *gesture,
 
   priv->active_row = NULL;
   priv->active_row_active = FALSE;
-  gtk_widget_queue_draw (GTK_WIDGET (box));
 
   g_object_unref (box);
 }
